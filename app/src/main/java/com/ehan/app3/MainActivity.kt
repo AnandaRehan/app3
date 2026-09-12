370149206618cee52905f8f41db4af136f64dda9
AnandaRehan <anandarehan071@gmail.com>
2026-09-12 19:53:29 +0700

    fix: 2

--- app/src/main/java/com/ehan/app3/MainActivity.kt
+++ app/src/main/java/com/ehan/app3/MainActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.weight
+// import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Scaffold
