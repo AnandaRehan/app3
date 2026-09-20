b55eb79b89d9f7ef942f15f736103380c2af0419
AnandaRehan <anandarehan071@gmail.com>
2026-09-20 14:50:53 +0700

    fix: 18

--- app/src/main/java/com/ehan/app3/MainActivity.kt
+++ app/src/main/java/com/ehan/app3/MainActivity.kt
@@ -45,7 +45,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.rememberLazyListState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
