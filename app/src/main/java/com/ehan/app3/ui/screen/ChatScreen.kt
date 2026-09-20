7f6c7d6c3eabf7ac274a002270ee835bcdff1439
AnandaRehan <anandarehan071@gmail.com>
2026-09-20 20:30:24 +0700

    fix: 20

--- app/src/main/java/com/ehan/app3/ui/screen/ChatScreen.kt
+++ app/src/main/java/com/ehan/app3/ui/screen/ChatScreen.kt
@@ -25,6 +25,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -47,7 +49,7 @@ fun ChatScreen(
 
     // State input
     var inputText by rememberSaveable {
-        androidx.compose.runtime.mutableStateOf("")
+        mutableStateOf("")
     }
 
     // Network monitor
