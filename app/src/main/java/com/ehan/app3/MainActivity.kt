26477998a7dc2a93d905ca37cb9e903bc7e5e2cd
AnandaRehan <anandarehan071@gmail.com>
2026-09-20 14:45:37 +0700

    fix: 17

--- app/src/main/java/com/ehan/app3/MainActivity.kt
+++ app/src/main/java/com/ehan/app3/MainActivity.kt
@@ -42,7 +42,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberLazyListState
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -150,7 +154,7 @@ fun ChatScreen() {
         mutableStateOf("")
     }
 
-    val messages = remember {
+    val messages = rememberSaveable {
         mutableStateListOf(
             ChatMessage(
                 text = "Halo! Saya bot sederhana 🤖",
