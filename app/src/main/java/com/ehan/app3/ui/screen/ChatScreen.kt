1ef6a5c2542a6ba770eb26402a68226fd0dc18b1
AnandaRehan <anandarehan071@gmail.com>
2026-09-21 09:24:59 +0700

    fix: 21

--- app/src/main/java/com/ehan/app3/ui/screen/ChatScreen.kt
+++ app/src/main/java/com/ehan/app3/ui/screen/ChatScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Icon
